0231fdee9a0d538834fe535495239b9bfb673d83
Hystabec <edward14577777@gmail.com>
2024-04-24 17:22:55 +0100

    Fixed namespace error in "TNT"

--- Assets/Scripts/Harrison/TNTScript.cs
+++ Assets/Scripts/Harrison/TNTScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TNT : MonoBehaviour
+public class TNTScript : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
