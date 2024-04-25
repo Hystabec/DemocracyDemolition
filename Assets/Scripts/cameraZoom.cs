21f4a831d382f688bf4948838ec7bbec678e5581
N1alllll <124519240+N1alllll@users.noreply.github.com>
2024-04-25 11:01:40 +0100

    Camera animations

---
+++ Assets/Scripts/cameraZoom.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraZoom : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] elementsToDisable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
