1a474dcdda8522ab3546af9d772a9196b111388e
Hystabec <114989230+Hystabec@users.noreply.github.com>
2024-05-02 15:39:27 +0100

    trampoline prefab and script done

--- Assets/Scripts/TrampolineScript.cs
+++ Assets/Scripts/TrampolineScript.cs
@@ -4,25 +4,15 @@ using UnityEngine;
 
 public class TrampolineScript : MonoBehaviour
 {
-    public void CustomOnTriggerEnter2D(Collider2D collision)
+    public void CustomOnCollisionEnter2D(Collision2D collision)
     {
-
-        if(collision.gameObject.layer == LayerMask.NameToLayer("projectile"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("projectile"))
         {
-            //reflect projectile
-            var projRB = collision.gameObject.GetComponent<Rigidbody2D>();
-
-            var collisionPoint = collision.ClosestPoint(transform.position);
-            var collisionNormal = transform.position - new Vector3(collisionPoint.x, collisionPoint.y);
-
-            ReflectProj(projRB, collisionNormal);
-            //play animation
+            if (typeof(PolygonCollider2D) == collision.otherCollider.GetType())
+            {
+                //Debug.Log("Bounce");
+                //play animation
+            }
         }
-    }
-
-    void ReflectProj(Rigidbody2D projectile, Vector2 reflectVector)
-    {
-        var _velocity = Vector2.Reflect(projectile.velocity, reflectVector);
-        projectile.velocity = _velocity;
     }
 }
