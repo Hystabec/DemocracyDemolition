2637f3f22a46f9e02a9eb059df52fff3414f0867
N1alllll <124519240+N1alllll@users.noreply.github.com>
2024-05-20 21:52:17 +0100

    Fade to/from black when pressing play, also triggered crowd particle effects when pressing play

--- Assets/Scripts/Harrison/Scene loader.cs
+++ Assets/Scripts/Harrison/Scene loader.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Sceneloader : MonoBehaviour
 {
+    [SerializeField]
+    Animator fadeAnim;
+
+    [SerializeField]
+    ParticleSystem redConfetti, blueConfetti, redStreamers, blueStreamers;
+    //Not sure why I can't .Play() when its an array[]
+
     public void PlayGame()
     {
-        Debug.Log("play");
-        SceneManager.LoadScene("protoDragDrop");
+        redConfetti.Play();
+        blueConfetti.Play();
+        redStreamers.Play();
+        blueStreamers.Play();
+        StartCoroutine(AnimsBeforePlay());
     }
     public void QuitGame()
     {
@@ -17,5 +27,13 @@ public class Sceneloader : MonoBehaviour
     public void Menu()
     {
         SceneManager.LoadScene("Harrison 2");
+    }
+
+    public IEnumerator AnimsBeforePlay()
+    {
+        yield return new WaitForSeconds(1.5f);
+        fadeAnim.SetTrigger("FadeToBlack");
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene("protoDragDrop");
     }
 }
